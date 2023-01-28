ecc61033ada1794b816c77698fbbfd9868fab13e
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2023-01-28 10:02:02 +0200

    tests refactored
    .gitignore updated

--- src/test/java/user/UserIT.java
+++ src/test/java/user/UserIT.java
@@ -1,12 +1,10 @@
 package user;
 
 import com.google.gson.JsonObject;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import utility.ConfigFileReader;
 
 import static io.restassured.RestAssured.given;
@@ -18,59 +16,45 @@ public class UserIT {
 
     static String token;
 
-    static String BASE_URL_USER = configReader.getBaseUrlUser();
-
-    static String BASE_URL_LOGIN = configReader.getBaseUrlLogin();
-
-    static String email;
+    static String userEmail;
 
     @BeforeClass
-    static void setUp() {
+    @Parameters({"email", "password"})
+    static void setUp(String email, String password) {
+
+        //Set up the base URI
+        RestAssured.baseURI = configReader.getBaseUrlUser();
 
         JsonObject jsonObj = new JsonObject();
-        jsonObj.addProperty("email", "jellofirsthand@gaa1iler.site");
-        jsonObj.addProperty("password", "qqqqq111");
+        jsonObj.addProperty("email", email);
+        jsonObj.addProperty("password", password);
 
+        //Create a new token based on the email and password of an existing user
         token = given().
                 contentType(ContentType.JSON)
                 .header("Content-Type", "application/json")
-                .body(jsonObj.toString()).when().post(BASE_URL_LOGIN)
+                .body(jsonObj.toString()).when().post(configReader.getBaseUrlLogin())
                 .then().statusCode(200).extract().header("Authorization");
-
-        email = generateEmail();
-
-        JsonObject newUser = new JsonObject();
-        newUser.addProperty("firstName", "test_firstName");
-        newUser.addProperty("lastName", "test_lastName");
-        newUser.addProperty("password", "qwerty1234");
-        newUser.addProperty("phoneNumber", "0742000000");
-        newUser.addProperty("email", email);
-        newUser.addProperty("deliveryAddress", "street, no. 1");
-
-        given().contentType(ContentType.JSON)
-                .body(newUser.toString())
-                .when()
-                .post(BASE_URL_USER)
-                .then()
-                .statusCode(201);
     }
 
 
-   @Test
+   @Test(priority=1)
     void testAddUser() {
+
+       userEmail = generateEmail();
 
         JsonObject jsonObj = new JsonObject();
         jsonObj.addProperty("firstName", "test_firstName");
         jsonObj.addProperty("lastName", "test_lastName");
         jsonObj.addProperty("password", "qwerty1234");
         jsonObj.addProperty("phoneNumber", "0742000000");
-        jsonObj.addProperty("email", generateEmail());
+        jsonObj.addProperty("email", userEmail);
         jsonObj.addProperty("deliveryAddress", "street, no. 1");
 
         given().contentType(ContentType.JSON)
                 .body(jsonObj.toString())
                 .when()
-                .post(BASE_URL_USER)
+                .post()
                 .then()
                 .assertThat().body("firstName", is("test_firstName"))
                 .and().body("lastName", is("test_lastName"))
@@ -80,29 +64,30 @@ public class UserIT {
     }
 
     @Test
-    void testDeleteUserByByEmail(){
-
-        given()
-                .header("Authorization", token)
-                .header("Content-Type", "application/json")
-                .pathParam("email", email)
-                .when()
-                .delete(BASE_URL_USER   + "{email}")
-                .then()
-                .assertThat().statusCode(200);
-
-    }
-
-    @Test
     @Parameters({"email"})
     void testGetUserByEmail(String email) {
+
         given().header("Authorization", token)
                 .queryParam("email", email)
-                .when().get(BASE_URL_USER)
+                .when().get()
                 .then().assertThat().statusCode(200);
     }
 
+
+    @Test(priority=2)
+    void testDeleteUserByEmail(){
+
+        given().contentType(ContentType.JSON)
+                .header("Authorization", token)
+                .header("Content-Type", "application/json")
+                .pathParam("email", userEmail)
+                .when()
+                .delete( "{email}")
+                .then()
+                .assertThat().statusCode(200);
+    }
+
     private static String generateEmail() {
-        return  RandomStringUtils.randomAlphanumeric(25) + "@gmail.com";
+        return  RandomStringUtils.randomAlphanumeric(25) + "@springstore-test.com";
     }
 }
